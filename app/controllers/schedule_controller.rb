d85ce6e9d9efab49d375a3e32c0f32902d3aee73
vmakoed <vmakoed@gmail.com>
2015-08-14 21:27:01 +0300

    Improved parsing and started building table view

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -4,6 +4,8 @@ class ScheduleController < ApplicationController
 
   WEEKDAYS = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
 
+  WEEK_NUMBERS = ["1", "2", "3", "4"]
+
   def index
   end
 
@@ -26,17 +28,25 @@ class ScheduleController < ApplicationController
 
   def form_hash(raw_hash)
     schedule_hash = Hash.new
-    raw_hash.each_with_index do |(weekday_hash), index|
-      schedule_hash[WEEKDAYS[index]] = form_weekday_hash(weekday_hash["schedule"])
+    WEEK_NUMBERS.each do |week_number|
+      schedule_hash[week_number] = form_week_hash(raw_hash, week_number)
     end
     schedule_hash
   end
 
-  def form_weekday_hash(raw_weekday_hash)
+  def form_week_hash(raw_hash, week_number)
+    week_hash = Hash.new
+    raw_hash.each_with_index do |(weekday_hash), index|
+      week_hash[WEEKDAYS[index]] = form_weekday_hash(weekday_hash["schedule"], week_number)
+    end
+    week_hash
+  end
+
+  def form_weekday_hash(raw_weekday_hash, week_number)
     if does_weekday_have_multiple_lessons?(raw_weekday_hash)
-      form_weekday_hash_for_multiple_lessons(raw_weekday_hash)
+      form_weekday_hash_for_multiple_lessons(raw_weekday_hash, week_number)
     else
-      form_weekday_hash_for_single_lesson(raw_weekday_hash)
+      form_weekday_hash_for_single_lesson(raw_weekday_hash, week_number)
     end
   end
 
@@ -44,26 +54,37 @@ class ScheduleController < ApplicationController
     raw_weekday_hash.kind_of?(Array)
   end
 
-  def form_weekday_hash_for_multiple_lessons(raw_weekday_hash)
+  def form_weekday_hash_for_multiple_lessons(raw_weekday_hash, week_number)
     weekday_hash = {}
     raw_weekday_hash.each do |lesson_hash|
-      weekday_hash["#{lesson_hash["numSubgroup"]}"] ||= []
-      weekday_hash["#{lesson_hash["numSubgroup"]}"] << form_weekday_hash_for_single_lesson(lesson_hash)
+      add_lesson_to_weekday_hash(weekday_hash, lesson_hash, week_number)
     end
     weekday_hash
   end
 
-  def form_weekday_hash_for_single_lesson(raw_weekday_hash)
-    weekday_hash = {}
-    weekday_hash["#{raw_weekday_hash["numSubgroup"]}"] = form_lesson_hash(raw_weekday_hash)
+  def add_lesson_to_weekday_hash(weekday_hash, lesson_hash, week_number)
+    weekday_hash["#{lesson_hash["numSubgroup"]}"] ||= {}
+    weekday_hash["#{lesson_hash["numSubgroup"]}"]["#{lesson_hash["lessonTime"]}"] ||= []
+    weekday_hash["#{lesson_hash["numSubgroup"]}"]["#{lesson_hash["lessonTime"]}"] << form_lesson_hash(lesson_hash, week_number)
+    weekday_hash["#{lesson_hash["numSubgroup"]}"]["#{lesson_hash["lessonTime"]}"].compact!
   end
 
-  def form_lesson_hash(raw_lesson_hash)
-    lesson_hash = Hash.new
-    lesson_hash["#{raw_lesson_hash["lessonTime"]}"] = {
-      "subject" => "#{raw_lesson_hash["subject"]}",
-      "lessonType" =>  "#{raw_lesson_hash["lessonType"]}"
-    }
+  def form_weekday_hash_for_single_lesson(raw_weekday_hash, week_number)
+    weekday_hash = {}
+    weekday_hash["#{raw_weekday_hash["numSubgroup"]}"] = form_lesson_hash(raw_weekday_hash, week_number)
+  end
+
+  def form_lesson_hash(raw_lesson_hash, week_number)
+    if raw_lesson_hash["weekNumber"].include? week_number
+      lesson_hash = Hash.new
+      lesson_hash = {
+        "subject" => "#{raw_lesson_hash["subject"]}",
+        "lessonType" => "#{raw_lesson_hash["lessonType"]}",
+        "auditory" =>  "#{raw_lesson_hash["auditory"]}"
+      }
+    else
+      lesson_hash = nil
+    end
     lesson_hash
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def weekdays
+    ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
+  end
+
+  def lesson_time_boundaries
+    ["08:00-09:35", "09:45-11:20", "11:40-13:15", "13:25-15:00", "15:20-16:55", "17:05-18:40"]
+  end
+
+  def week_numbers
+    ["1", "2", "3", "4"]
+  end
 end
