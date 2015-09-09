951334af89bc3ba25fd78fae114c2d53af9115d6
Victor Makoed <vmakoed@gmail.com>
2015-09-09 23:26:20 +0300

    Fixed problems with group numbers containing Russian letters

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Set Encoding
+Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the Rails application.
 Rails.application.initialize!
