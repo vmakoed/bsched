ccc6a7804cb63c61a6e74ac09f7114b9e32e476e
Victor Makoed <vmakoed@gmail.com>
2015-09-25 22:42:47 +0300

    Implemented minor design changes and done refactoring with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,31 +3,15 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
-  def weekdays
+  def get_weekdays
     ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
   end
 
-  def lesson_time_boundaries
+  def get_lesson_time_boundaries
     ["08:00-09:35", "09:45-11:20", "11:40-13:15", "13:25-15:00", "15:20-16:55", "17:05-18:40", "18:45-20:20"]
   end
 
-  def week_numbers
+  def get_week_numbers
     ["1", "2", "3", "4"]
-  end
-
-  def lesson_container(lesson_type)
-    "lesson-container lesson-container-#{classes_by_lesson_types[lesson_type]}"
-  end
-
-  def badge_current(lesson_type)
-    "badge badge-current badge-current-#{classes_by_lesson_types[lesson_type]}"
-  end
-
-  def classes_by_lesson_types
-    {
-      "ЛК" => "lecture",
-      "ПЗ" => "seminar",
-      "ЛР" => "lab"
-    }
   end
 end
--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,2 +1,66 @@
 module ScheduleHelper
+  def lesson_container(lesson_type)
+    "lesson-container lesson-container-#{classes_by_lesson_types[lesson_type]}"
+  end
+
+  def badge_current(lesson_type)
+    "badge badge-current badge-current-#{classes_by_lesson_types[lesson_type]}"
+  end
+
+  def classes_by_lesson_types
+    {
+      "ЛК" => "lecture",
+      "ПЗ" => "seminar",
+      "ЛР" => "lab"
+    }
+  end
+
+  def th_class_for_time_boundary(current_time, lesson_time_boundary)
+    if current_time >= lesson_time_boundary[0, 5] && current_time <= lesson_time_boundary[-5, 5]
+      "class = th-current-lesson"
+    else
+      nil
+    end
+  end
+
+  def tr_class_for_week(week_number, current_week)
+    if week_number == current_week
+      "tr-current-week tr-current-week-header"
+    else
+      "weekday-tr"
+    end
+  end
+
+  def tr_class_for_day(week_number, current_week, weekday, current_day)
+    weekdays = get_weekdays
+    if week_number == current_week
+      if weekday == weekdays[current_day - 1]
+        "class = tr-current-day"
+      else
+        "class = tr-current-week"
+      end
+    else
+      nil
+    end
+  end
+
+  def does_lesson_exist?(schedule_hash, week_number, weekday, lesson_time_boundary)
+    does_lesson_exist = false
+    if schedule_hash[week_number][weekday]
+      if schedule_hash[week_number][weekday][lesson_time_boundary]
+        if time_has_lessons = schedule_hash[week_number][weekday][lesson_time_boundary].any?
+          does_lesson_exist = true
+        end
+      end
+    end
+    does_lesson_exist
+  end
+
+  def is_current_time?(current_time, lesson_time_boundary)
+    current_time >= lesson_time_boundary[0, 5] && current_time <= lesson_time_boundary[-5, 5]
+  end
+
+  def is_current_lesson?(current_time, lesson_time_boundary, week_number, current_week, weekday, current_day)
+    is_current_time?(current_time, lesson_time_boundary) && week_number == current_week && weekday == weekdays[current_day - 1]
+  end
 end
