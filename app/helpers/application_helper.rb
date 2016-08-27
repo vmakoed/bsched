824382590e98a65164ba0b1c970e7bd78a09be4e
Victor Makoed <vmakoed@gmail.com>
2016-08-27 13:49:10 +0300

    Add missing lesson time boundary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   @@weekdays = %w(ПН ВТ СР ЧТ ПТ СБ)
-  @@lesson_time_boundaries = %w(08:00-09:35 09:45-11:20 11:40-13:15 13:25-15:00 15:20-16:55 17:05-18:40 18:45-20:20)
+  @@lesson_time_boundaries = %w(08:00-09:35 09:45-11:20 11:40-13:15 13:25-15:00 15:20-16:55 17:05-18:40 18:45-20:20 20:25-22:00)
   @@week_numbers = %w(1 2 3 4)
 
   def title(page_title)
